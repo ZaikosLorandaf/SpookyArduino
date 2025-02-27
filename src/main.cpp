b4f41b03c001ba43abebc176a3b114e84b55fe6a
Simon Labrie <simonlabrie@icloud.com>
2025-02-27 13:59:44 -0500

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 int x, y, z;
 ////////////////////////////////////////////////////////////////////////
 //Joystick
-const int VRX_PIN = A2; // Arduino pin connected to VRX pin
+const int VRX_PIN = A4; // Arduino pin connected to VRX pin
 const int VRY_PIN = A3;// Arduino pin connected to VRY pin
 
 int xValue = 0; // To store value of the X axis
@@ -175,7 +175,7 @@ void loop(){
     lcd.setCursor(1,0);
   }
 
-  /*x = analogRead(A8);       // read analog input pin A4
+  x = analogRead(A8);       // read analog input pin A4
   y = analogRead(A9);       // read analog input pin A5
   z = analogRead(A10);       // read analog input pin A6
   Serial.print(x, DEC);    // print the acceleration in the X axis
@@ -183,7 +183,7 @@ void loop(){
   Serial.print(y, DEC);    // print the acceleration in the Y axis
   Serial.print(" ");       // prints a space between the numbers
   Serial.println(z, DEC);  // print the acceleration in the Z axis
-  delay(250);*/
+  delay(250);
 
   pot_Value = analogRead(POT_INPUT);
   int barLevel = map(pot_Value, 0, 1023, 0, BARGRAPHE_SIZE);
@@ -205,7 +205,7 @@ void loop(){
           }
   } 
       
-      Serial.println(analogRead(A0));
+      //Serial.println(analogRead(A0));
 
 
 
